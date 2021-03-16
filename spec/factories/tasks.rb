868ac83e63815b6454cc98789b02e56afd4e783c
masa-oh <makun0084pc@gmail.com>
2021-03-16 15:46:27 +0900

    [add]02_モデルスペック

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:title) { |n| "テスト#{n}" }
+    content {'これはテストです。'}
+    status {0}
+    deadline {Time.new}
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '全ての属性があれば有効であること' do
+    expect(FactoryBot.build(:task)).to be_valid
+  end
+
+  it 'タイトル無しでは無効であること' do
+    task = FactoryBot.build(:task, title: nil)
+    task.valid?
+    expect(task.errors[:title]).to include("can't be blank")
+  end
+
+  it 'ステータス無しでは無効であること' do
+    task = FactoryBot.build(:task, status: nil)
+    task.valid?
+    expect(task.errors[:status]).to include("can't be blank")
+  end
+
+  it '重複したタイトルでは無効であること' do
+    FactoryBot.create(:task, title: 'テスト')
+    task = FactoryBot.build(:task, title: 'テスト')
+    task.valid?
+    expect(task.errors[:title]).to include('has already been taken')
+  end
+
+  it '別のタイトルで有効なタスクが作成できること' do
+    FactoryBot.create(:task, title: 'Test')
+    task = FactoryBot.build(:task, title: 'Another Test')
+    expect(task).to be_valid
+  end
 end
