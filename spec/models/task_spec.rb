2de3ba0fcdc853b92827c33d9abda20fea86117f
masa-oh <makun0084pc@gmail.com>
2021-03-18 00:41:52 +0900

    [Fix]有効な場合、エラーがないことの確認項目を追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,32 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it '全ての属性があれば有効であること' do
-    expect(FactoryBot.build(:task)).to be_valid
-  end
+  describe 'バリデーション' do
+    it '全ての属性があれば有効であること' do
+      task = FactoryBot.build(:task)
+      expect(task).to be_valid
+      expect(task.errors).to be_empty
+    end
 
-  it 'タイトル無しでは無効であること' do
-    task = FactoryBot.build(:task, title: nil)
-    task.valid?
-    expect(task.errors[:title]).to include("can't be blank")
-  end
+    it 'タイトル無しでは無効であること' do
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
 
-  it 'ステータス無しでは無効であること' do
-    task = FactoryBot.build(:task, status: nil)
-    task.valid?
-    expect(task.errors[:status]).to include("can't be blank")
-  end
+    it 'ステータス無しでは無効であること' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
 
-  it '重複したタイトルでは無効であること' do
-    FactoryBot.create(:task, title: 'テスト')
-    task = FactoryBot.build(:task, title: 'テスト')
-    task.valid?
-    expect(task.errors[:title]).to include('has already been taken')
-  end
+    it '重複したタイトルでは無効であること' do
+      FactoryBot.create(:task, title: 'テスト')
+      task = FactoryBot.build(:task, title: 'テスト')
+      task.valid?
+      expect(task.errors[:title]).to include('has already been taken')
+    end
 
-  it '別のタイトルで有効なタスクが作成できること' do
-    FactoryBot.create(:task, title: 'Test')
-    task = FactoryBot.build(:task, title: 'Another Test')
-    expect(task).to be_valid
+    it '別のタイトルで有効なタスクが作成できること' do
+      FactoryBot.create(:task, title: 'Test')
+      task = FactoryBot.build(:task, title: 'Another Test')
+      expect(task).to be_valid
+      expect(task.errors).to be_empty
+    end
   end
 end
