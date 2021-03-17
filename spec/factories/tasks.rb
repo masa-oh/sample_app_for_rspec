49d01699924df50bd1ca7576d271f6ac17b326da
masa-oh <makun0084pc@gmail.com>
2021-03-18 00:33:30 +0900

    [Fix]Taskファクトリの属性を微修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :task do
     sequence(:title) { |n| "テスト#{n}" }
-    content {'これはテストです。'}
-    status {0}
-    deadline {Time.new}
+    content { "これはテストです。" }
+    status { :todo }
+    deadline { 1.week.from_now }
     association :user
   end
 end
