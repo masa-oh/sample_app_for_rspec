9d67904580e925bf94713d248f1b32126daeb8c2
masa-oh <makun0084pc@gmail.com>
2021-03-25 23:13:16 +0900

    [Add]Tasksに関するシステムテストを記述

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,16 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
+  let(:user) { create(:user) }
+  let(:new_task) { build(:task, user: user) }
+  let(:registered_task) { create(:task, user: user) }
+  let(:task_with_another_title) { build(:task, user: user) }
+  let(:another_user) { create(:user) }
+  let(:task_created_by_another_user) { create(:task, user: another_user) }
+
   describe 'ログイン前' do
     describe 'タスク新規作成' do
       context 'ログインしていない状態' do
-        it 'タスク新規作成ページへのアクセスが失敗する'
+        it 'タスク新規作成ページへのアクセスが失敗する' do
+          visit new_task_path
+          expect(page).to have_current_path '/login'
+          expect(page).to have_content 'Login required'
+        end
       end
     end
 
     describe 'タスク編集' do
       context 'ログインしていない状態' do
-        it 'タスク編集ページへのアクセスが失敗する'
+        it 'タスク編集ページへのアクセスが失敗する' do
+          visit edit_task_path(registered_task)
+          expect(page).to have_current_path '/login'
+          expect(page).to have_content 'Login required'
+        end
       end
     end
   end
@@ -18,22 +33,51 @@ RSpec.describe "Tasks", type: :system do
   describe 'ログイン後' do
     describe 'タスク新規作成' do
       context 'フォームの入力値が正常' do
-        it 'タスクの新規作成が成功する'
+        it 'タスクの新規作成が成功する' do
+          login(user)
+          visit new_task_path
+          fill_in 'task[title]', with: new_task.title
+          fill_in 'task[content]', with: new_task.content
+          select 'doing', from: 'task[status]'
+          fill_in 'task[deadline]', with: 1.week.from_now
+          click_button 'Create Task'
+          expect(page).to have_content 'Task was successfully created.'
+        end
       end
     end
 
     describe 'タスク編集' do
       context 'フォームの入力値が正常' do
-        it 'タスクの編集が成功する'
+        it 'タスクの編集が成功する' do
+          login(user)
+          visit edit_task_path(registered_task)
+          fill_in 'task[title]', with: task_with_another_title.title
+          click_button 'Update Task'
+          expect(page).to have_content 'Task was successfully updated.'
+        end
       end
+
       context '他ユーザーのタスク編集ページにアクセス' do
-        it 'タスク編集ページへのアクセスが失敗する'
+        it 'タスク編集ページへのアクセスが失敗する' do
+          login(user)
+          visit edit_task_path(task_created_by_another_user)
+          expect(page).to have_content 'Forbidden access.'
+        end
       end
     end
 
     describe 'タスク削除' do
       context 'フォームの入力値が正常' do
-        it 'タスクの削除が成功する'
+        let!(:task_to_be_destroyed) { create(:task, user: user) }
+
+        it 'タスクの削除が成功する' do
+          login(user)
+          visit tasks_path
+          page.accept_confirm do
+            click_on 'Destroy', match: :first
+          end
+          expect(page).to have_content 'Task was successfully destroyed.'
+        end
       end
     end
   end
