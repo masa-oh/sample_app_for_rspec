b6fdc047eff1281250266a9ad470e301f79576ed
masa-oh <makun0084pc@gmail.com>
2021-03-30 04:05:35 +0900

    [Fix]Usersスペックの確認項目を詳細化

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe "Users", type: :system do
   describe 'ログイン前' do
     describe 'ユーザー新規登録' do
-      let(:user) { build(:user) }
+      let(:new_user) { build(:user) }
       let(:registered_user) { create(:user) }
+      before { visit sign_up_path }
 
       context 'フォームの入力値が正常' do
         it 'ユーザーの新規作成が成功する' do
-          visit sign_up_path
-          fill_in 'user[email]', with: user.email
+          fill_in 'user[email]', with: new_user.email
           fill_in 'user[password]', with: 'password'
           fill_in 'user[password_confirmation]', with: 'password'
           click_button 'SignUp'
@@ -20,7 +20,6 @@ RSpec.describe "Users", type: :system do
 
       context 'メールアドレスが未入力' do
         it 'ユーザーの新規作成が失敗する' do
-          visit sign_up_path
           fill_in 'user[email]', with: ''
           fill_in 'user[password]', with: 'password'
           fill_in 'user[password_confirmation]', with: 'password'
@@ -32,7 +31,6 @@ RSpec.describe "Users", type: :system do
 
       context '登録済のメールアドレスを使用' do
         it 'ユーザーの新規作成が失敗する' do
-          visit sign_up_path
           fill_in 'user[email]', with: registered_user.email
           fill_in 'user[password]', with: 'password'
           fill_in 'user[password_confirmation]', with: 'password'
@@ -62,8 +60,8 @@ RSpec.describe "Users", type: :system do
     describe 'ユーザー編集' do
       let(:user) { create(:user) }
       let(:user_to_be_updated) { build(:user) }
-      let(:another_user) { create(:user) }
-      let(:user_with_duplicated_title) { build(:user, email: another_user.email) }
+      let(:other_user) { create(:user) }
+      let(:user_with_duplicated_title) { build(:user, email: other_user.email) }
 
       context 'フォームの入力値が正常' do
         it 'ユーザーの編集が成功する' do
@@ -84,7 +82,8 @@ RSpec.describe "Users", type: :system do
           fill_in 'user[password]', with: 'pass'
           fill_in 'user[password_confirmation]', with: 'pass'
           click_button 'Update'
-          expect(page).to_not have_content 'User was successfully updated.'
+          expect(page).to have_content('1 error prohibited this user from being saved')
+          expect(page).to have_content("Email can't be blank")
           expect(current_path).to eq user_path(user)
         end
       end
@@ -96,15 +95,15 @@ RSpec.describe "Users", type: :system do
           fill_in 'user[password]', with: 'pass'
           fill_in 'user[password_confirmation]', with: 'pass'
           click_button 'Update'
-          expect(page).to_not have_content 'User was successfully updated.'
+          expect(page).to have_content('1 error prohibited this user from being saved')
+          expect(page).to have_content('Email has already been taken')
           expect(current_path).to eq user_path(user)
         end
       end
       
       context '他ユーザーの編集ページにアクセス' do
         it '編集ページへのアクセスが失敗する' do
-          visit edit_user_path(another_user)
-          expect(page).to have_current_path "/users/#{user.id}"
+          visit edit_user_path(other_user)
           expect(page).to have_content 'Forbidden access.'
           expect(current_path).to eq user_path(user)
         end
@@ -117,7 +116,12 @@ RSpec.describe "Users", type: :system do
       context 'タスクを作成' do
         it '新規作成したタスクが表示される' do
           visit user_path(user)
+          expect(page).to have_content('You have 1 task.')
           expect(page).to have_content task.title
+          expect(page).to have_content task.status
+          expect(page).to have_link('Show')
+          expect(page).to have_link('Edit')
+          expect(page).to have_link('Destroy')
         end
       end
     end
